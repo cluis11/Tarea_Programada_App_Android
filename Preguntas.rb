fb46581f4a87c69c60f3051de702a47d1f5abab8
cluis11 <cluis.9405@gmail.com>
2014-11-20 04:37:33 -0600

    Update Preguntas.rb
    Actualización del archivo

--- Preguntas.rb
+++ Preguntas.rb
@@ -1,32 +1,87 @@
-[08:32:43 p.m.] Luis Castro: class Preguntas
- def initialize()
-  File.open('preguntas.txt', 'a')
- end
+class Preguntas
 
- def preguntas_()
-  @temp=""
-  File.open('preguntas.txt','r') do |f|
-   while line = f.gets
-    @temp=@temp+line
-   end
-  end
-  @temp
- end
+	def initialize()
+		File.open('preguntas.txt', 'a')
+	end
 
- def nueva_pregunta(preg) 
-  @id=self.genera_id()
-  File.open('preguntas.txt', 'a') do |f|
-     f.puts @id+"+"+preg
-  end
-  preg
- end
+	def preguntas_(fn)
+		temp=""
+		File.open('preguntas.txt','r') do |f|
+			while line = f.gets
+				if fn
+					temp+=self.formato(line)+" "
+				else
+					temp+=line+" "
+				end
+			end
+		end
+		temp
+	end
 
- def genera_id()
-  @temp=self.preguntas_()
-  @temp=@temp.split(" ")
-  @temp=@temp.last
-  @temp=@temp.split("+")
-  @temp=@temp.first.to_i+1
-  @temp.to_s
- end
+	def formato(preg)
+		cont=0
+		temp=""
+		str=""
+		preg=preg.split("+")
+		preg.each {|u|
+			if cont==2
+				u.each_char {|c| 
+					if c=="&"
+						temp+="["+str+"]"
+						str=""
+					else 
+						str+=c
+					end
+				}
+			else
+				temp+="["+u+"]" 
+			end
+			cont+=1
+		}
+		temp
+	end
+
+	def nueva_pregunta(preg)	
+		id=self.genera_id()
+		id=id.to_s
+		temp=id.to_s+"+"+preg
+		File.open('preguntas.txt', 'a') do |f|
+  			f.puts temp
+		end
+		preg
+	end
+
+	def genera_id()
+		temp=self.preguntas_(false)
+		temp=temp.split(" ")
+		temp=temp.last
+		temp=temp.split("+")
+		puts temp.first
+		temp=temp.first.to_i+1
+		temp
+	end
+
+	def ultimas_preguntas()
+		ultimas=""
+		tempL=[]
+		temp=self.genera_id()
+		ultimas=self.preguntas_(true)
+		if temp.to_i>5
+			ultimas=ultimas.split(" ")
+			ultimas.each { |u| tempL.push(u.split("+")) }
+			ultimas=""
+			cont=0
+			File.open('preguntas.txt','r') do |f|
+				while line = f.gets
+					if tempL[cont][0].to_i>=temp.to_i-5
+						ultimas+=line
+					end
+					cont+=1
+				end
+			end
+		else
+			ultimas=self.preguntas_(true)
+		end
+		ultimas
+	end
 end
