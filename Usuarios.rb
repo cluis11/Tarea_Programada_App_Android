faead0de7b2d5a0a85ea265497e3a93517c8be4d
cluis11 <cluis.9405@gmail.com>
2014-11-20 04:38:56 -0600

    Update Usuarios.rb
    Actualizacion del archivo

--- Usuarios.rb
+++ Usuarios.rb
@@ -1,33 +1,41 @@
 class Usuarios 
- 
- def initialize()
-  File.open('usuarios.txt', 'a')
- end
+	
+	def initialize()
+		File.open('usuarios.txt', 'a')
+	end
 
- def usuarios_()
-  @temp=""
-  File.open('usuarios.txt','r') do |f|
-   while line = f.gets
-    @temp=@temp+line
-    @temp=@temp+"#"
-   end
-  end
-  @temp=@temp.gsub(/\s+/, "")
-  @temp
- end
+	def usuarios_()
+		temp=""
+		File.open('usuarios.txt','r') do |f|
+			while line = f.gets
+				temp+=line
+				temp+="#"
+			end
+		end
+		temp=temp.gsub(/\s+/, "")
+		temp
+	end
 
- def nuevo_usuario(user)
-  @temp="" 
-  user.each_char {|u|
-   if u=="_"
-    @temp=@temp+","
-   else
-    @temp=@temp+u
-   end
-  }
-  File.open('usuarios.txt', 'a') do |f|
-     f.puts @temp
-  end
-  user
- end
+	def validar(user)
+		tempL=[]
+		temp=self.usuarios_()
+		temp=temp.split("#")
+		temp.each { |u| tempL.push(u.split(",")) }
+		temp=user.split(",")
+	end
+
+	def nuevo_usuario(user)
+		temp=""	
+		user.each_char {|u|
+			if u=="_"
+				temp=temp+","
+			else
+				temp=temp+u
+			end
+		}
+		File.open('usuarios.txt', 'a') do |f|
+  			f.puts temp
+		end
+		user
+	end
 end
