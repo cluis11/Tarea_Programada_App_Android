f1dc9e4de60fa12d5aaf0a317f756564ef850d39
cluis11 <cluis.9405@gmail.com>
2014-11-20 04:39:55 -0600

    Update app.rb
    actualizacion del documento

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require "sinatra"
+
 require "./Usuarios.rb"
 require "./Preguntas.rb"
 require "./Respuestas.rb"
@@ -7,32 +9,45 @@ $P = Preguntas.new()
 $R = Respuestas.new()
 
 get '/' do                                  
- "Prueba"
+	ultimas=$P.ultimas_preguntas()
+	ultimas
 end
 
 get '/usuarios' do
- @user=$U.usuarios_()
- @user
+	user=$U.usuarios_()
+	user
 end
 
 get '/get_usuario/:user' do
- @temp=params[:user]
- @user=$U.nuevo_usuario(@temp)
- @user
+	temp=params[:user]
+	user=$U.nuevo_usuario(temp)
+	user
 end
 
 get '/preguntas' do
- @preg=$P.preguntas_()
- @preg
+	preg=$P.preguntas_(true)
+	preg
 end
 
 get '/get_pregunta/:preg' do
- @temp=params[:preg]
- @preg=$P.nueva_pregunta(@temp)
- @preg
+	temp=params[:preg]
+	preg=$P.nueva_pregunta(temp)
+	preg
 end
 
 get '/respuestas' do
- @res=$R.respuestas_()
- @res
+	res=$R.respuestas_(true)
+	res
+end
+
+get '/get_respuesta/:res' do
+	temp=params[:res]
+	res=$R.nueva_respuesta(temp)
+	res
+end
+
+get '/respuestas/:id' do
+	temp=params[:id]
+	asociadas=$R.respuestas_asociadas(temp)
+	asociadas
 end
